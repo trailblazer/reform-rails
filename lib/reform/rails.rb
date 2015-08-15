9a5b6a879c888aa3b704b28e637377f20b7c508d
Nick Sutterer <apotonick@gmail.com>
2015-08-15 19:22:45 +1000

    releasing 0.1.0.

--- lib/reform/rails.rb
+++ lib/reform/rails.rb
@@ -6,7 +6,7 @@ require "reform/form/active_model/validations"
 require "reform/active_record" if defined?(ActiveRecord)
 require "reform/mongoid" if defined?(Mongoid)
 
-Reform::Form.class_eval do # DISCUSS: i'd prefer having a separate Rails module to be mixed into the Form but this is way more convenient for 99% users.
+Reform::Form.class_eval do
   include Reform::Form::ActiveModel
   include Reform::Form::ActiveModel::FormBuilderMethods
   include Reform::Form::ActiveRecord if defined?(ActiveRecord)
