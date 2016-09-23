90e4a11fd3310b7c86572c90e73c8429631f3668
Fran Worley <frances@safetytoolbox.co.uk>
2016-09-23 11:26:26 +0100

    remove unused dummy-app files

--- test/rails-app/config/environments/development.rb
+++ test/rails-app/config/environments/development.rb
@@ -13,4 +13,5 @@ Dummy::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
   config.secret_key_base = 123
+  config.eager_load = false
 end
