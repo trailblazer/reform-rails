737abf061bfaf07ef81042d508aa6207149e5aeb
Nick Sutterer <apotonick@gmail.com>
2017-02-18 17:17:55 +0100

    don't include the module twice.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,10 +4,6 @@ require "rails-app/config/environment"
 require 'reform/rails'
 
 require "reform/form/active_model/validations"
-Reform::Contract.class_eval do
-  feature Reform::Form::ActiveModel::Validations
-end
-# FIXME!
 Reform::Form.class_eval do
   feature Reform::Form::ActiveModel::Validations
 end
